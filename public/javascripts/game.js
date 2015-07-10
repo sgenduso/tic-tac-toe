d00b05903b0f7ebe2673d41703970e33b0387e37
Sam Genduso <sam.genduso@gmail.com>
2015-07-10 13:18:46 -0600

    checks for winner

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,10 +1,41 @@
 var tileArray = document.getElementsByClassName('tile');
 var tileValue = 'X';
 
-[].forEach.call(tileArray, function (e) {
+var checkWinner = function (tileArray) {
+  for (var i = 0; i < tileArray.length; i+=3) {
+    if (tileArray[i].innerHTML === tileArray[i+1].innerHTML && tileArray[i].innerHTML === tileArray[i+2].innerHTML && tileArray[i].innerHTML !== '') {
+      alert('WINNA WINNA');
+      tileValue = 'O';
+      [].forEach.call(tileArray, function (e) {
+        e.innerHTML = '';
+      });
+    }
+  }
+  for (var i = 0; i < 3; i++) {
+    if (tileArray[i].innerHTML === tileArray[i+3].innerHTML && tileArray[i].innerHTML === tileArray[i+6].innerHTML && tileArray[i].innerHTML !== '') {
+      alert('WINNA WINNA');
+      tileValue = 'O';
+      [].forEach.call(tileArray, function (e) {
+        e.innerHTML = '';
+      });
+    }
+  }
+  for (var i = 0; i < 3; i+=2) {
+    if(tileArray[i].innerHTML === tileArray[4].innerHTML && tileArray[i].innerHTML === tileArray[8-i].innerHTML && tileArray[i].innerHTML !== '') {
+      alert('WINNA WINNA');
+      tileValue = 'O';
+      [].forEach.call(tileArray, function (e) {
+        e.innerHTML = '';
+      });
+    }
+  }
+};
+
+[].forEach.call(tileArray, function (e, i, a) {
   e.addEventListener('click', function() {
     if (e.innerHTML === ""){
-      e.innerHTML = tileValue;
+      this.innerHTML = tileValue;
+      checkWinner(a);
       tileValue = tileValue === 'O' ? 'X' : 'O';
     } else {
       alert('This tile has already been filled, bitch.');
